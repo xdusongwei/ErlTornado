82b4b6851780d68d45d1336072aeb996433c5561
宋伟 <github@songwei.io>
2015-01-26 19:20:55 +0800

    Update cookie.erl

--- cookie.erl
+++ cookie.erl
@@ -2,6 +2,7 @@
 -compile(export_all).
 
 % auth(<<"tornado key here">>, <<"cookie value here">>).
+% auth(<<"YXRvbS50cmFkZSBpcyB">>,<<"2|1:0|10:1422245002|4:user|44:MTUwMjkwMjQxMTJ8MTEwM3wxfDUwMDAwMXwzMDAwMDE=|356c025f8ea244f188dad2345446732be1b17376a701bab9503022214088b61e">>).
 auth(Key, Value)->
 	[H1, H2, TS, K, V, S] = re:split(erlang:binary_to_list(Value),"\\|", [{return,list}]),
 	Origin = lists:flatten([H1, "|", H2, "|", TS, "|", K, "|", V, "|"]),
